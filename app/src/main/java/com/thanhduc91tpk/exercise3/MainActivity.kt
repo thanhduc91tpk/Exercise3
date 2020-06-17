6623d47ecf98aba227c5b5def28c3adb243c4595
admin <thanhduc91tpk@gmail.com>
2020-06-17 18:17:15 +0700

    Initial Commit

---
+++ app/src/main/java/com/thanhduc91tpk/exercise3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.thanhduc91tpk.exercise3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
